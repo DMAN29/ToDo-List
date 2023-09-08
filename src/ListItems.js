33445ca2e135f30871a473bd0205a304c7438856
Debabrat Ranjan <ranjan.dr29@gmail.com>
2023-09-08 17:01:16 +0530

    responsiveness fixed

--- src/ListItems.js
+++ src/ListItems.js
@@ -7,7 +7,7 @@ const ListItems = (props) => {
   };
   return (
     <div className='listItems'>
-    <li style={{textDecoration : line ? "line-through":"none"}}>{props.val.name}</li>
+    <li style={{textDecoration : line ? "line-through":"none",paddingLeft:"0.4rem",textAlign:"left"}}>{props.val.name}</li>
     <div className="btns">
       <button className='btn btn-green' style={{display: line ? "none":"inline"}} onClick={done}>👍</button>
       <button className='btn btn-green' style={{display: line ? "none":"inline"}} onClick={()=>{props.onClick("edit",props.val.id)}}>✏️</button>
